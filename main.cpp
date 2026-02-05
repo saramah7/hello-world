0e54df55b441960c863604ae2a68a65ef7b465c7
Sara Toori <saratoori7@gmail.com>
2026-02-04 23:56:37 -0500

    Add interactive greeting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello world." << std::endl;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    std::cout << "Hello, " << name << "." << std::endl;
     return 0;
 }
